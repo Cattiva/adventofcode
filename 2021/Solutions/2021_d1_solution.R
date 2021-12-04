197e82feafe96f5a2113a20a89315059af3bc89b
Cattiva <alexander.nickel@mailbox.org>
2021-12-04 10:23:55 +0100

    Day 4

--- 2021/Solutions/2021_d1_solution.R
+++ 2021/Solutions/2021_d1_solution.R
@@ -6,7 +6,7 @@ input <- as.integer(readLines(input_path))
 
 # Part 1 ------------------------------------------------------------------
 
-get_in_or_decrease <- function(input){
+get_in_or_decrease <- function(input) {
 
   result <- dplyr::as_tibble(input) %>% 
   dplyr::mutate(previous_depth = dplyr::lag(value), #Take previous value
@@ -25,11 +25,12 @@ get_in_or_decrease(input)
 
 # Part 2 ------------------------------------------------------------------
 
-#First calculate the new 3-measure rolling window (could surely use something like Rcpp::roll_sum, too)
-new_depth <- function(measurement_no){
+#First calculate the new 3-measure rolling window 
+#(could surely use something like Rcpp::roll_sum, too)
+new_depth <- function(measurement_no) {
   sum(input[measurement_no:(measurement_no+2)])
 }
 
 new_measures <- purrr::map_dbl(1:(length(input-2)), new_depth) 
 
-get_in_or_decrease(new_measures) #reuse the first function
+solution2 <- get_in_or_decrease(new_measures) #reuse the first function
